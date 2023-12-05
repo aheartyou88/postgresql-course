277e4b672023c8d171f897f1a0c0f5a403515b13
Amanda You <aheartyou88@gmail.com>
2023-12-05 01:21:07 -0800

    ninja lvl 8 exercises

--- 090/02-create-table-people.sql
+++ 090/02-create-table-people.sql
@@ -1,12 +1,3 @@
-CREATE TABLE people( 
-    pid SERIAL PRIMARY KEY,
-    pFirstName VARCHAR(50) NOT NULL,
-    plastname VARCHAR(50) NOT NULL,
-    pcity VARCHAR(50) NOT NULL,
-    pstate VARCHAR(2) NOT NULL,
-    pdob TIMESTAMP NOT NULL,
-    pjoined TIMESTAMP NOT NULL,
-    plastvisit TIMESTAMP NOT NULL,
-    ppoints INT NOT NULL,
-    pfavorites JSONB NOT NULL
-);
+CREATE TABLE people 
+( pid SERIAL PRIMARY KEY,
+pFirstName VARCHAR(50) NOT NULL);
